8314ca27596838678ee321ed629e25225d9a6893
Stas Fomin <stas-fomin@yandex.ru>
2019-02-01 19:43:43 +0300

    Fixes to make it works behind the firewall (default keyserver port not available)

--- util/install/ansible-bootstrap.sh
+++ util/install/ansible-bootstrap.sh
@@ -47,7 +47,7 @@ VIRTUAL_ENV="/tmp/bootstrap"
 PYTHON_BIN="${VIRTUAL_ENV}/bin"
 ANSIBLE_DIR="/tmp/ansible"
 CONFIGURATION_DIR="/tmp/configuration"
-EDX_PPA_KEY_SERVER="keyserver.ubuntu.com"
+EDX_PPA_KEY_SERVER="hkp://keyserver.ubuntu.com:80"
 EDX_PPA_KEY_ID="B41E5E3969464050"
 
 cat << EOF
@@ -147,7 +147,7 @@ if [[ "true" == "${RUN_ANSIBLE}" ]]; then
 
     # Install the configuration repository to install
     # edx_ansible role
-    git clone ${CONFIGURATION_REPO} ${CONFIGURATION_DIR}
+    git clone ${CONFIGURATION_REPO} ${CONFIGURATION_DIR} || true
     cd ${CONFIGURATION_DIR}
     git checkout ${CONFIGURATION_VERSION}
     make requirements
